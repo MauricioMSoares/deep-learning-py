40d44764e0f486de24ce15a56dbc4b0877dc185b
Mauricio Soares <mauricio.massayuki@gmail.com>
2025-12-31 00:09:26 -0300

    added scikit LabelEncoder function

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 dataset = pd.read_csv("Churn_Modelling.csv")
+print("\nRaw dataset")
 print(dataset)
 
 x = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+print("\nX division")
 print(x)
+print("\nY division")
 print(y)
+
+encode_gender = LabelEncoder()
+
+x[:, 2] = encode_gender.fit_transform(x[:, 2])
+
+print("\nGender encoded X division")
+print(x)
