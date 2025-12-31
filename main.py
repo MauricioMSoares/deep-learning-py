b33bbb2435748e07754df9ed8e7fc577ae684e9a
Mauricio Soares <mauricio.massayuki@gmail.com>
2025-12-31 00:42:00 -0300

    added OneHotEncoder, and test train transform functions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 
 
 dataset = pd.read_csv("Churn_Modelling.csv")
@@ -22,3 +24,21 @@ x[:, 2] = encode_gender.fit_transform(x[:, 2])
 
 print("\nGender encoded X division")
 print(x)
+
+ct = ColumnTransformer([("ohe", OneHotEncoder(), [1])], remainder="passthrough")
+x = np.array(ct.fit_transform(x), dtype=np.str_)
+x = x[:, 1:]
+
+print("\nCountry encoded X division")
+print(x)
+
+sc = StandardScaler()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+# y_train = sc.fit_transform(y_train)
+# y_test = sc.transform(y_test)
+
+x_train_df = pd.DataFrame(x_train)
+print("\nX Dataframes after train and test transform")
+print(x_train_df)
